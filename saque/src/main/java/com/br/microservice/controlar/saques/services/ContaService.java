e7beafcbbde77e61a52d2b91acf001b4393a670a
Osito Flor <netojose42@gmail.com>
2023-03-14 09:35:21 -0300

    Atualizado pasta

--- saque/src/main/java/com/br/microservice/controlar/saques/services/ContaService.java
+++
@@ -1,8 +0,0 @@
-package com.br.microservice.controlar.saques.services;
-
-import com.br.microservice.controlar.saques.dtos.SolicitacaoDto;
-
-public interface ContaService {
-
-    void validaSaque(SolicitacaoDto solicitacao);
-}
